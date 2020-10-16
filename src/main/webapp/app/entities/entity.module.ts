cd27bf1a6939c7141b5a7cf4b7cab4fcf9a21b90
Julien Bideau <julien.bideau-prestataire@laposte.fr>
2020-10-16 11:34:05 +0200

    Update entities + add phpmyadmin on mysql.yml

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,38 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'region',
+        loadChildren: () => import('./region/region.module').then(m => m.TechawayRegionModule)
+      },
+      {
+        path: 'country',
+        loadChildren: () => import('./country/country.module').then(m => m.TechawayCountryModule)
+      },
+      {
+        path: 'location',
+        loadChildren: () => import('./location/location.module').then(m => m.TechawayLocationModule)
+      },
+      {
+        path: 'department',
+        loadChildren: () => import('./department/department.module').then(m => m.TechawayDepartmentModule)
+      },
+      {
+        path: 'task',
+        loadChildren: () => import('./task/task.module').then(m => m.TechawayTaskModule)
+      },
+      {
+        path: 'employee',
+        loadChildren: () => import('./employee/employee.module').then(m => m.TechawayEmployeeModule)
+      },
+      {
+        path: 'job',
+        loadChildren: () => import('./job/job.module').then(m => m.TechawayJobModule)
+      },
+      {
+        path: 'job-history',
+        loadChildren: () => import('./job-history/job-history.module').then(m => m.TechawayJobHistoryModule)
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
